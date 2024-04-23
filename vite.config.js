5de29c775e7e6acb848956e1b11a2299ba57dfce
mata99000 <141237915+mata99000@users.noreply.github.com>
2024-04-24 01:41:08 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+   server:{
+    https: true,
+   },
+   
     plugins: [
         laravel({
             input: [
