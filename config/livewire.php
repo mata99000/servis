aee8c6efa3435bd9036111394c0cb62b35e3ede2
mata99000 <141237915+mata99000@users.noreply.github.com>
2024-04-24 17:03:54 +0200

    image/png

--- config/livewire.php
+++ config/livewire.php
@@ -69,6 +69,7 @@ return [
         'directory' => 'tmp',   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
+            'image/png', 'image/gif', 'image/bmp', 'image/svg', 'image/webp', 'image/jpeg', 'image/jpg',
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
